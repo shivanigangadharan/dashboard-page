cdac04332ef554d6260559340404798498efad17
shivanigangadharan <shivani.gangadharan@gmail.com>
2023-03-13 22:04:15 +0530

    folder structure

---
+++ src/dashboard/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/footer/footer';
+import Navbar from '../components/navbar/navbar';
+
+export default function Dashboard() {
+    return (
+        <>
+            <Navbar />
+            dashboard
+            <Footer />
+        </>
+    )
+}

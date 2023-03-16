5bac0442d52f5e27558815eeb59c6132a58899d1
shivanigangadharan <shivani.gangadharan@gmail.com>
2023-03-16 23:00:45 +0530

    Added empty box

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -8,47 +8,72 @@ import Box from '@mui/material/Box';
 import Select from '@mui/material/Select';
 import './dashboard.css';
 import axios from 'axios';
+import Skeleton from '@mui/material/Skeleton';
+import MoodIcon from '@mui/icons-material/Mood';
+import MoodBadIcon from '@mui/icons-material/MoodBad';
 
 export default function Dashboard() {
     const [feed, setFeed] = useState('');
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const GetNewsReports = async (provider) => {
+        setLoading(true)
         try {
             const res = await axios.get(`https://biz-api.text-miner.com/finfeed/${provider}`)
             let result = [];
-            JSON.parse(res.data).map(async (item, i) => {
-                console.log("waiting")
-                if (i < 5) {
-                    const itemArray = item.split("\t");
-                    let emotion = "";
-                    let currObject = {
-                        text: itemArray[0],
-                        date: itemArray[1]
-                    }
-                    const response = await axios.post(
-                        'https://api.text-miner.com/sentiment',
-                        `message=${currObject.text}`,
-                        {
-                            headers: {
-                                'Content-Type': 'application/x-www-form-urlencoded'
-                            }
+            await Promise.allSettled(
+                JSON.parse(res.data).map(async (item, i) => {
+                    if (i < 5) {
+                        const itemArray = item.split("\t");
+                        let emotion = "";
+                        let currObject = {
+                            text: itemArray[0],
+                            date: itemArray[1],
+                            emo: []
                         }
-                    );
-                    let neg = response.data.negative_sentiment_percentage;
-                    let pos = response.data.positive_sentiment_percentage;
-                    if (neg > pos) {
-                        emotion = "Happy"
-                    } else {
-                        emotion = "Sad"
+                        const response = await axios.post(
+                            'https://api.text-miner.com/sentiment',
+                            `message=${currObject.text}`,
+                            {
+                                headers: {
+                                    'Content-Type': 'application/x-www-form-urlencoded'
+                                }
+                            }
+                        );
+                        let neg = response.data.negative_sentiment_percentage;
+                        let pos = response.data.positive_sentiment_percentage;
+                        if (neg > pos) {
+                            emotion = "Happy"
+                        } else {
+                            emotion = "Sad"
+                        }
+                        currObject = { ...currObject, emotion: emotion }
+                        result.push(currObject)
                     }
-                    currObject = { ...currObject, emotion: emotion }
-                    result.push(currObject)
-                    console.log('co: ', currObject)
-                }
-            })
-            console.log("result array: ", result)
+                })
+            )
             setItems(result)
+            setLoading(false)
+            // let emoResult = [];
+            // await Promise.allSettled(
+            //     items.map(async (item) => {
+            //         let currObject;
+            //         const emoRes = await axios.post(
+            //             'https://api.text-miner.com/ner',
+            //             `message=${item.text}`,
+            //             {
+            //                 headers: {
+            //                     'Content-Type': 'application/x-www-form-urlencoded'
+            //                 }
+            //             }
+            //         )
+            //         console.log(emoRes.data)
+            //         currObject = { ...item, entities: emoRes.data }
+            //         emoResult.push(currObject)
+            //     })
+            // )
+            // setItems(emoResult)
         } catch (error) {
             console.error(error)
         }
@@ -60,8 +85,6 @@ export default function Dashboard() {
         const provider = event.target.value;
         GetNewsReports(provider);
     };
-
-    console.log("items: ", items)
 
     return (
         <div className='dashboard-container'>
@@ -84,21 +107,35 @@ export default function Dashboard() {
                     </Select>
                 </FormControl>
             </Box>
-            <Box className="items-container">
-                {items.map((item, i) => {
-                    return <Box key={i} className="item-card">
-                        <span> {item?.text} </span>
-                        <Box className="subtext">
-                            <div className='date'> {item?.date} </div>
-                            <div> {item?.emotion} </div>
+            {
+                !loading && items.length === 0 ?
+                    <Box className="empty-box">{'Choose a feed provider to view sentimental analysis of news articles.'} </Box> :
+                    (loading ?
+                        [1, 2, 3].map((e) => {
+                            return <Box className="skeletons-container">
+                                <Skeleton className='skeleton' animation="wave" variant="rectangular" height={100} />
+                            </Box>
+                        })
+                        :
+                        <Box className="items-container">
+                            {items.map((item, i) => {
+                                return <Box key={i} className="item-card">
+                                    <span> {item?.text} </span>
+                                    <Box className="subtext">
+                                        <div className='date'> {item?.date} </div>
+                                        <div> {item?.emotion === "Happy" ? <MoodIcon sx={{ color: "green" }} /> : <MoodBadIcon sx={{ color: "crimson" }} />} </div>
+                                    </Box>
+                                    <Box sx={{ display: "flex" }}>
+                                        Named entities: {item?.entities?.map((e) => {
+                                            return <Box> {e} </Box>
+                                        })}
+                                    </Box>
+                                </Box>
+                            })}
                         </Box>
-                        <Box>
-                            Named entities:
-                        </Box>
-                    </Box>
-                })}
+                    )
+            }
 
-            </Box>
 
         </div>
     )
